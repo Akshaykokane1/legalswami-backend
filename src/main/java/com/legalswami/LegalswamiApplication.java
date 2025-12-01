39e7b5b5f6c8d3ce1cd404d1828c85000138b1d2
Akshaykokane1 <44544586+Akshaykokane1@users.noreply.github.com>
2025-12-01 08:11:03 +0530

    Update LegalswamiApplication.java

--- src/main/java/com/legalswami/LegalswamiApplication.java
+++ src/main/java/com/legalswami/LegalswamiApplication.java
@@ -2,10 +2,11 @@ package com.legalswami;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.legalswami"})
 public class LegalswamiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LegalswamiApplication.class, args);
     }
